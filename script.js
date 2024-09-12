39e5071a999e50018b6c874e697cbc82dfc19c9d
berktutu <166663599+berktutu@users.noreply.github.com>
2024-09-13 00:35:08 +0200

    Created a user input logic and computer choice logic

---
+++ script.js
@@ -0,0 +1,86 @@
+console.log("Hello World!");
+
+// Create a function for the math.random logic
+// Logic of computer's choice Rock, Paper, Scissors
+// It should be a function so it returns one of the above.
+// Create a prompt for user's pick
+// Declare scores
+
+// Computer Choice and Human Choice Logic //
+
+// Fixing user input (whitespace, uppercase and lowercase)
+const fixInput = function (str) {
+  let trimmedStr = str.trim();
+  if (typeof trimmedStr !== "string" || trimmedStr.length === 0)
+    return "Wrong input";
+  return trimmedStr[0].toUpperCase() + trimmedStr.slice(1).toLowerCase();
+};
+
+const getHumanChoice = function () {
+  const humanInput = prompt(
+    "Enter your choice: Rock, Paper, or Scissors (The input is not case sensetive)"
+  );
+  const fixedHumanInput = fixInput(humanInput);
+  const inputLog = "Your choice:" + " " + fixedHumanInput;
+  console.log(inputLog);
+  return fixedHumanInput;
+};
+
+// Generating number between 1-3 for computer choice
+const generateNumber = function (min, max) {
+  const generatedNum = Math.floor(Math.random() * (max - min + 1) + min);
+  return generatedNum;
+};
+
+// Computer choice functionality
+const choices = {
+  1: "Rock",
+  2: "Paper",
+  3: "Scissors",
+};
+const getComputerChoice = function () {
+  const propertyValue = generateNumber(1, 3);
+  const computerChoice = choices[propertyValue];
+  // Getting the choice according to property name.
+  console.log(`Computer's choice: ${computerChoice}`);
+  return computerChoice;
+};
+
+// Game Logic
+let humanScore = 0;
+let computerScore = 0;
+
+// Getting the values of the function
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+// Getting the outcome with possibilites of win, lose and draw
+const outcomes = {
+  Rock: { Paper: "Lose", Scissors: "Win", Rock: "Draw" },
+  Paper: { Rock: "Win", Scissors: "Lose", Paper: "Draw" },
+  Scissors: { Rock: "Lose", Paper: "Win", Scissors: "Draw" },
+};
+function getOutcome(humanOutcome, computerOutcome) {
+  return outcomes[humanOutcome][computerOutcome];
+}
+
+// Function for console message depending on the outcome
+function getMessage(humanOutput, computerOutput) {
+  const outcome = getOutcome(humanOutput, computerOutput);
+
+  if (outcome === "Lose") {
+    return `You ${outcome}! ${computerOutput} beats ${humanOutput}`;
+  }
+  if (outcome === "Win") {
+    return `You ${outcome}! ${humanOutput} beats ${computerOutput}`;
+  } else {
+    return `It's a Draw!`;
+  }
+}
+
+// Round function calling the getMessage function to declare who wins
+const playRound = function (humanChoice, computerChoice) {
+  console.log(getMessage(humanChoice, computerChoice));
+};
+
+playRound(humanSelection, computerSelection);
